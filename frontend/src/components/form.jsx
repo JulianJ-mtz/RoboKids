b96d9a242062e74290ebe89a4306108fac27b4ac
Julianj-mtz <a01254258@tec.mx>
2024-02-13 17:00:35 -0700

    .

---
+++ frontend/src/components/form.jsx
@@ -0,0 +1,11 @@
+export default function Formulario() {
+    return (
+        <>
+            <container class="mt-4"> 
+
+                hola, que tal 
+
+            </container>
+        </>
+    );
+}
