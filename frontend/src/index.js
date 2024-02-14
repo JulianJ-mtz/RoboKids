d2131ef5ac1ee5b69910d82748bc3d572f15c103
Julianj-mtz <a01254258@tec.mx>
2024-02-13 17:02:03 -0700

    .

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,10 +4,14 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import Formulario from './components/form';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <Formulario/>
+
   </React.StrictMode>
 );
 
